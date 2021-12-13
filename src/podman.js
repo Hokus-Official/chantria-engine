34d9d0810a72c76637436f7e8528d954a5d26a29
oddmario <mariolatif741@yandex.com>
2021-12-13 15:31:48 +0000

    Update podman wrapper

--- src/podman.js
+++ src/podman.js
@@ -1,7 +1,34 @@
+const axios = require('axios');
+const PODMAN_UNIX_SOCKET = "/run/podman/podman.sock";
+
 module.exports = {
     
-    createContainer: function (name, image, ports, memory) {
-        
+    createContainer: async function (data) {
+        const requiredFields = [
+            "name",
+            "image"
+        ];
+        for(const field of requiredFields) {
+            if(!data[field]) {
+                throw "Missing the required field '" + field + "'";
+            }
+        }
+        await axios({
+            socketPath: PODMAN_UNIX_SOCKET,
+            url: "/v3.0.0/libpod/containers/create",
+            method: "POST",
+            data: {
+                image: data['image'],
+                name: data['name']
+            }
+        }).then(function (response) {
+            return Promise.resolve({
+                id: response['Id'],
+                warnings: response['Warnings']
+            });
+        }).catch(function (error) {
+            throw "An error has occured while creating the container";
+        });
     }
     
 };
